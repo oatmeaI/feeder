f0488139bd3f8834d0aabc1dc7ff3a5476dda902
keenan <keenan.vh@icloud.com>
2023-02-09 11:14:11 -0800

    add a bunch more sources, cleaned up some logic

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 import express from "express";
 import path from "path";
 import process from "process";
-import { JSDOM } from "jsdom";
+import jsdom, { JSDOM } from "jsdom";
 const app = express();
 const port = 3000;
+
+const virtualConsole = new jsdom.VirtualConsole();
+virtualConsole.on("error", () => {
+    // No-op to skip console errors.
+});
 
 function scrub(text) {
     return text?.replaceAll(/[\t\n]/g, "");
@@ -14,21 +19,24 @@ async function fetchSite({
     itemSelector,
     titleSelector,
     dateSelector,
+    dateAttribute,
     descriptionSelector,
     urlSelector,
     imageSelector,
     url: siteUrl,
     name,
 }) {
-    const req = await fetch(siteUrl);
-    const res = await req.text();
-    const doc = new JSDOM(res).window.document;
+    // const req = await fetch(siteUrl);
+    // const res = await req.text();
+    // const doc = new JSDOM(res).window.document;
+    const doc = (await JSDOM.fromURL(siteUrl, { virtualConsole })).window.document;
 
     const items = doc.querySelectorAll(itemSelector);
 
     return Array.from(items).map((item) => {
         const title = scrub(item.querySelector(titleSelector)?.textContent);
-        const rawDate = scrub(item.querySelector(dateSelector)?.textContent);
+        const dateElement = item.querySelector(dateSelector);
+        const rawDate = scrub(dateAttribute ? dateElement?.getAttribute(dateAttribute) : dateElement?.textContent);
         const url = urlSelector && scrub(item.querySelector(urlSelector)?.href);
         const image = scrub(item.querySelector(imageSelector)?.src);
         const description = descriptionSelector ? scrub(item.querySelector(descriptionSelector)?.textContent) : "";
@@ -71,32 +79,96 @@ const sites = [
         name: "Great American",
     },
     {
-        itemSelector: "#sessionsByDateConent .film",
-        titleSelector: ".title",
-        dateSelector: ".date",
+        itemSelector: ".eventlist-event--upcoming",
+        titleSelector: ".eventlist-title",
+        dateSelector: ".eventlist-meta-date",
         imageSelector: ".poster",
-        dateMod: (d) => `${d} ${new Date().getFullYear()}`,
-        url: "https://ticketing.uswest.veezi.com/sessions/?siteToken=d2atbcege5knqsavntt91g1250",
+        urlSelector: ".eventlist-title-link",
+        dateMod: (d) => d.replace(/\w,/, ""),
+        url: "https://www.4-star-movies.com/calendar-of-events",
         name: "4-Star Theater",
     },
     {
-        itemSelector: "li .chakra-linkbox",
+        itemSelector: ".css-13o7eu2 li > div.chakra-linkbox > a.chakra-linkbox__overlay",
         titleSelector: "li .chakra-linkbox header h3",
         dateSelector: "li .chakra-linkbox time", // TODO - this breaks when the time is "tomorrow" - but there's timetamp in the attributes of the element
         imageSelector: "li .chakra-linkbox img",
         urlSelector: "li .chakra-linkbox a",
-        dateMod: (d) => `${d} ${new Date().getFullYear()}`,
+        dateAttribute: "datetime",
         url: "https://www.livenation.com/venue/KovZpZAJ6nlA/the-masonic-events",
         name: "The Masonic",
+    },
+    {
+        itemSelector: ".content-information",
+        titleSelector: ".show-title",
+        dateSelector: ".date-show",
+        dateAttribute: "content",
+        imageSelector: ".img-responsive",
+        urlSelector: ".entry a",
+        url: "https://billgrahamcivic.com/event-listing/",
+        name: "Bill Graham",
+    },
+    {
+        itemSelector: ".event-item",
+        titleSelector: ".event-title",
+        dateSelector: ".event-date",
+        dateMod: (d) => `${d} ${new Date().getFullYear()}`,
+        imageSelector: ".event-thumb img",
+        urlSelector: ".event-title a",
+        url: "https://publicsf.com/calendar/",
+        name: "Public Works",
+    },
+    {
+        itemSelector: ".entry",
+        titleSelector: ".carousel_item_title_small",
+        dateSelector: ".date",
+        imageSelector: ".thumb img",
+        urlSelector: ".carousel_item_title_small a",
+        descriptionSelector: ".title h4",
+        url: "https://www.theregencyballroom.com/events",
+        name: "The Regency",
+    },
+    {
+        itemSelector: "div:not(.postponed-event) > .tw-event-item",
+        titleSelector: ".tw-name a",
+        descriptionSelector: ".tw-support",
+        dateSelector: ".tw-event-date",
+        dateMod: (d) => d?.replaceAll(".", "/") + "/" + new Date().getFullYear(),
+        imageSelector: ".tw-image img",
+        urlSelector: ".tw-name a",
+        url: "http://theindependentsf.com/",
+        name: "The Independent",
+    },
+    {
+        itemSelector: "div[data-testid=organizer-profile__future-events] .eds-show-up-mn .eds-event-card-content",
+        titleSelector: ".eds-event-card-content__title",
+        dateSelector: ".eds-event-card-content__sub-title",
+        dateMod: (d) =>
+            d.includes("Tomorrow")
+                ? new Date().setDate(new Date().getDate() + 1)
+                : d.replace(/\w,/, "").replace(/, .*/, "") + " " + new Date().getFullYear(),
+        imageSelector: ".eds-event-card-content__image-wrapper img",
+        urlSelector: ".eds-event-card-content__primary-content a",
+        url: "https://www.eventbrite.com/o/neck-of-the-woods-18588469785",
+        name: "Neck of the Woods",
     },
 ];
 
 async function getItems() {
     const results = [];
-    for (const site of sites) {
-        results.push(...(await fetchSite(site)));
-    }
-    const sortedResults = results.sort((a, b) => new Date(a.date) - new Date(b.date));
+    await Promise.all(
+        sites.map(async (site) => {
+            const start = Date.now();
+            console.log(`Fetching ${site.name}...`);
+            results.push(...(await fetchSite(site)));
+            const end = Date.now();
+            const time = (end - start) / 1000;
+            console.log(`${site.name} fetched. Took ${time} seconds.`);
+        })
+    );
+    const sortedResults = results
+        .filter((i) => new Date(i.date) >= new Date().setDate(new Date().getDate() - 1))
+        .sort((a, b) => new Date(a.date) - new Date(b.date));
     return sortedResults;
 }
 
